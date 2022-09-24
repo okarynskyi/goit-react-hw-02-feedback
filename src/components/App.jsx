57b7aa4de7c94725409a971d1341603f125b9f7f
Oleh Okarynskyi <okarynskyy@gmail.com>
2022-09-24 21:35:52 +0300

    step1

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,42 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
+import { Component } from 'react';
+
+
+export class App extends Component {
+  state = {
+    good: 0,
+    neutral: 0,
+    bad: 0
+  }
+
+  handleIncrement = (e) => {
+    const { name } = e.currentTarget;
+    this.setState((prevState) => ({
+      [name]: prevState[name] + 1,
+    }))
+  }
+  
+  render() {
+    const { good, neutral, bad } = this.state;
+    const options = ['good', 'neutral', 'bad'];
+    const buttons = options.map(option => {
+      return <button key={option} name={option} onClick={this.handleIncrement}>{option}</button>
+    });
+
+    return (
+      <>
+        <div>
+          <h1>Please leave feedback</h1>
+          {buttons}
+        </div>
+        <div>
+          <h1>Statistics</h1>
+          <ul>
+            <li>Good: {good}</li>
+            <li>Neutral: {neutral}</li>
+            <li>Bad: {bad}</li>
+          </ul>
+        </div>
+      </>
+    )
+  }
 };
